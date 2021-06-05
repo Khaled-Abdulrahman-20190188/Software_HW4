d75570e1b240384bd9d9abf955f065c5fb3c8161
Khaled Abdulrahman <khaleedabdulrahman159@gmail.com>
2021-06-05 04:56:16 +0200

    this is the main class to test User class

---
+++ SW_HW4.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sw_hw4;
+
+public class SW_HW4 {
+
+    public static void main(String[] args) {
+       User Khaled=new User();
+       Khaled.Register("Khaled", 5, "Kjuio097", "zekomadred159@gmail.com", "01112896605", "Giza");
+       Khaled.Display();
+       User mohammed=new User();
+       mohammed.Register("mohammed", 5, "ooiokl9", "moha@gmail.com", "01007688950", "Cairo");
+       Khaled.Display();
+
+    }
+
+
+    
+    }
